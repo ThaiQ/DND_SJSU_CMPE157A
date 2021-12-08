bac3ed5eefa0c95f97c8211881a0b58390bb0c98
ThaiQ <quachhongthai200@gmail.com>
2021-12-07 19:24:55 -0800

    flask default call

--- frontend/src/dashboard/dashboard.js
+++ frontend/src/dashboard/dashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './dashboard.css'
 import Grid from './grid'
 import Drawer from '../components/left-navbar/drawer'
-import {char} from './data'
+import { char } from './data'
 
 const Chart = require('chart.js')
 
@@ -11,7 +11,7 @@ export default function Profile(props) {
     //Making sure that user is login
     const [user, setUser] = useState(localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : '');
     useEffect(() => {
-        
+
         console.log(user)
     }, [])
 
@@ -24,7 +24,7 @@ export default function Profile(props) {
     return (
         <div className='dashboard-app'>
             hi
-            <Drawer title='Dashboard' content={Grid} user={char}/>
+            <Drawer title='Dashboard' content={Grid} user={char} />
         </div>
     );
 }
--- frontend/src/dashboard/data.js
+++ frontend/src/dashboard/data.js
@@ -1,34 +1,54 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from "react-router-dom";
 
 const queryParams = new URLSearchParams(window.location.search);
 
-async function get(){
+async function get() {
     let player_data = null
     let bio_data = null
     let quipment_data = null
     let inventory_data = null
     let class_spell_data = null
+    let class_data = ["","Fighter"]
 
     //Fetch info from Flask server
-    await axios.get('http://localhost:5000',{
+    await axios.get('http://localhost:5000', {
         params: {
             playerID: queryParams.get('user')
         }
-      }).then((res)=>{
-        player_data = res.data.playerdata
-        bio_data = res.data.biodata
-        quipment_data = res.data.equipmentdata
-        inventory_data = res.data.inventorydata
-        class_spell_data = res.data.classspelldata 
+    }).then((res) => {
+        player_data = res.data[0]
+        bio_data = res.data[1]
+        quipment_data = res.data[6]
+        inventory_data = res.data[6]
+        class_spell_data = res.data[8]
+        class_data = res.data[5]
     })
 
-    let stats = [player_data.constitution, player_data.strength, player_data.dexterity, player_data.intelligence,
-        player_data.wisdom, player_data.charisma, 10]
-    player_data = `${player_data.name} | ${player_data.class} - ${player_data.level} | Exp: ${player_data.experience} `
+    let stats = [player_data[5], player_data[6], player_data[7], player_data[8], player_data[9], player_data[10], player_data[11]]
+    player_data = `${player_data[2]} | ${class_data[1]} - ${player_data[0]} | Exp: ${player_data[1]} `
     return {
-        player_data,bio_data,quipment_data,inventory_data,class_spell_data,stats
+        player_data, bio_data: {
+            bio: bio_data[2],
+            trait: bio_data[2],
+            ideal: bio_data[1],
+            flaw: bio_data[2]
+        },
+
+        quipment_data: [{
+            name: quipment_data[1],
+            desc: quipment_data[1],
+            stats: quipment_data[2]
+        }], 
+
+        inventory_data: [{
+            name: inventory_data[1],
+            desc: inventory_data[1],
+            stats: inventory_data[2]
+        }],
+        
+        class_spell_data: [class_spell_data[2]], stats
     }
 }
 
@@ -55,10 +75,10 @@ export var items = get().inventory_data || [
 
 export var eq_items = get().quipment_data || [
     {
-        slot:"Chestplate",
-        name:"Leather Armor",
-        desc:"Light Armor",
-        stats:"AC 10"
+        slot: "Chestplate",
+        name: "Leather Armor",
+        desc: "Light Armor",
+        stats: "AC 10"
     }
 ]
 
